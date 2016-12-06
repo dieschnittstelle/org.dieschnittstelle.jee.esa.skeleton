bb54858918df11df843ec99ab8bea6e362a7a8cd
jk <joern.kreutel@beuth-hochschule.de>
2016-12-06 17:40:01 +0100

    use REST service proxy for interacting with layers underneath of presentation as an alternative to ejb injection

--- org.dieschnittstelle.jee.esa.skeleton.ejbmodule/src/main/java/org/dieschnittstelle/jee/esa/skeleton/ejb/TodoCRUDStateless.java
+++ org.dieschnittstelle.jee.esa.skeleton.ejbmodule/src/main/java/org/dieschnittstelle/jee/esa/skeleton/ejb/TodoCRUDStateless.java
@@ -6,27 +6,33 @@ import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import org.apache.log4j.Logger;
 import org.dieschnittstelle.jee.esa.skeleton.jpa.Todo;
 
 @Stateless
 public class TodoCRUDStateless implements TodoCRUDLocal {
+
+	protected static Logger logger = Logger.getLogger(TodoCRUDStateless.class);
 	
 	@PersistenceContext(unitName="skeleton_PU")
 	private EntityManager em;
 
 	@Override
 	public Todo createTodo(Todo todo) {
+		logger.info("createTodo(): " + todo);
 		em.persist(todo);
 		return todo;
 	}
 
 	@Override
 	public List<Todo> readAllTodos() {
+		logger.info("readAllTodos()");
 		return em.createQuery("FROM Todo").getResultList();
 	}
 
 	@Override
 	public Todo readTodo(long id) {
+		logger.info("readTodo(): " + id);
 		return em.find(Todo.class, id);
 	}
 
--- org.dieschnittstelle.jee.esa.skeleton.webapp/src/main/java/org/dieschnittstelle/jee/esa/skeleton/webapp/jsf/TodoViewController.java
+++ org.dieschnittstelle.jee.esa.skeleton.webapp/src/main/java/org/dieschnittstelle/jee/esa/skeleton/webapp/jsf/TodoViewController.java
@@ -8,16 +8,25 @@ import javax.ejb.EJB;
 import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
 
+import org.apache.log4j.Logger;
 import org.dieschnittstelle.jee.esa.skeleton.ejb.TodoCRUDLocal;
 import org.dieschnittstelle.jee.esa.skeleton.jpa.Todo;
+import org.jboss.resteasy.client.jaxrs.ResteasyClient;
+import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
+import org.jboss.resteasy.client.jaxrs.ResteasyWebTarget;
 
 @ManagedBean(name="vc")
 @ApplicationScoped
 public class TodoViewController {
-	
-	
+
+	protected static Logger logger = Logger.getLogger(TodoViewController.class);
+
+	// this annotation injects an ejb proxy
 	@EJB
 	private TodoCRUDLocal todoCRUD;
+
+	// set this to true in case a REST service proxy shall be used, which will replace the ejb proxy
+	private boolean useRESTServiceProxy = false;
 
 	/*
 	 * this attribute holds data for a new todo instance that might be created via the UI
@@ -53,10 +62,28 @@ public class TodoViewController {
 	}
 	
 	
-	/*
-	 * this method initially creates two todos 
-	 */
 	@PostConstruct
+	public void initialise() {
+		if (useRESTServiceProxy) {
+			// 1. create a client
+			ResteasyClient client = (new ResteasyClientBuilder()).build();
+
+			// 2. create a ResteasyWebTarget from client
+			ResteasyWebTarget target = client.target("http://localhost:8080/org.dieschnittstelle.jee.esa.skeleton.webapp/api");
+
+			// 3. instantiate serviceClient using proxy() on target and passing interface
+			todoCRUD = target.proxy(TodoCRUDLocal.class);
+		}
+		logger.info("initialise(): using crud proxy: " + todoCRUD);
+
+		// create some dummy data
+		createInitialTodos();
+	}
+
+
+	/*
+	 * this method initially creates two todos
+	 */
 	public void createInitialTodos() {
 		this.todoCRUD.createTodo(new Todo("lorem","ipsum dolor"));
 		this.todoCRUD.createTodo(new Todo("dorem","lipsum olor"));
